93eb198b1226c05775438627de50cab4115c3e74
Vuong Tran <121814098+Vtrandev@users.noreply.github.com>
2023-07-11 00:11:31 -0700

    Minor typo edits

--- app/success/page.jsx
+++ app/success/page.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Success = () => {
   return (
     <div className='title flex flex-col items-center py-24'>
-        <h1>Thank you for your Application!</h1>
-        <h1>We will reach out to your soon.</h1>
+        <h1>Thank you for your application!</h1>
+        <h1>We will reach out to you soon.</h1>
     </div>
   )
 }
