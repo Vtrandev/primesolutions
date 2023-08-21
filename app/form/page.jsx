06f35ebab83af06fecaf8b574e543faa7fa6f70c
Vuong Tran <121814098+Vtrandev@users.noreply.github.com>
2023-08-21 16:10:11 -0700

    Progress bar added to form page

--- app/form/page.jsx
+++ app/form/page.jsx
@@ -72,8 +72,7 @@ const Form = () => {
 
       appendSpreadsheet(newRow);
       router.push("/success");
-    }
-    else alert("Submit failed. Check Name, Email, & Phone");
+    } else alert("Submit failed. Check Name, Email, & Phone");
   }
 
   const [step, setStep] = useState(1);
@@ -97,85 +96,92 @@ const Form = () => {
       >
         {/* ----------------------Step 1--------------- */}
         {step === 1 && (
-        <div className="card">
-          <label
-            className={`block ${errors.name && "text-red-600"} `}
-            htmlFor="name"
-          >
-            Name {errors.name && `(${errors.name.message})`}
-          </label>
-          <input
-            className="form"
-            type="text"
-            placeholder="Name"
-            id="name"
-            {...register("name", {
-              required: "Enter your name",
-              maxLength: 80,
-            })}
-          />
+          <div className="card">
+            <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
+              <div className="bg-blue-600 h-2.5 w-[33%] rounded-full"></div>
+            </div>
 
-          <label
-            className={`block ${errors.email && "text-red-600"} `}
-            htmlFor="email"
-          >
-            Email {errors.email && `(${errors.email.message})`}
-          </label>
-          <input
-            className="form"
-            type="text"
-            placeholder="Email"
-            id="email"
-            {...register("email", {
-              required: "Enter your email",
-              pattern: /^\S+@\S+$/i,
-            })}
-          />
+            <label
+              className={`block ${errors.name && "text-red-600"} `}
+              htmlFor="name"
+            >
+              Name {errors.name && `(${errors.name.message})`}
+            </label>
+            <input
+              className="form"
+              type="text"
+              placeholder="Name"
+              id="name"
+              {...register("name", {
+                required: "Enter your name",
+                maxLength: 80,
+              })}
+            />
 
-          <label
-            className={`block ${errors.number && "text-red-600"} `}
-            htmlFor="number"
-          >
-            Phone number {errors.number && `(${errors.number.message})`}
-          </label>
-          <input
-            className="form"
-            type="tel"
-            id="number"
-            placeholder="Mobile number"
-            {...register("number", {
-              required: "Enter your phone #",
-              minLength: 6,
-              maxLength: 15,
-            })}
-          />
+            <label
+              className={`block ${errors.email && "text-red-600"} `}
+              htmlFor="email"
+            >
+              Email {errors.email && `(${errors.email.message})`}
+            </label>
+            <input
+              className="form"
+              type="text"
+              placeholder="Email"
+              id="email"
+              {...register("email", {
+                required: "Enter your email",
+                pattern: /^\S+@\S+$/i,
+              })}
+            />
 
-          <label className="block" htmlFor="title">
-            Title
-          </label>
+            <label
+              className={`block ${errors.number && "text-red-600"} `}
+              htmlFor="number"
+            >
+              Phone number {errors.number && `(${errors.number.message})`}
+            </label>
+            <input
+              className="form"
+              type="tel"
+              id="number"
+              placeholder="Mobile number"
+              {...register("number", {
+                required: "Enter your phone #",
+                minLength: 6,
+                maxLength: 15,
+              })}
+            />
 
-          <select
-            className="form"
-            id="title"
-            {...register("title", { required: false })}
-          >
-            <option value="Mr">Mr</option>
-            <option value="Mrs">Mrs</option>
-            <option value="Miss">Miss</option>
-            <option value="Dr">Dr</option>
-            <option value="Other">Other</option>
-          </select>
-          <div className="text-center">
-            <button className="button" onClick={() => NextStep(step)}>
-              Next
-            </button>
+            <label className="block" htmlFor="title">
+              Title
+            </label>
+
+            <select
+              className="form"
+              id="title"
+              {...register("title", { required: false })}
+            >
+              <option value="Mr">Mr</option>
+              <option value="Mrs">Mrs</option>
+              <option value="Miss">Miss</option>
+              <option value="Dr">Dr</option>
+              <option value="Other">Other</option>
+            </select>
+            <div className="text-center">
+              <button className="button" onClick={() => NextStep(step)}>
+                Next
+              </button>
+            </div>
           </div>
-        </div>
         )}
 
         {/* ----------------------Step 2--------------- */}
         {step === 2 && (
           <div className="card">
+            <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
+              <div className="bg-blue-600 h-2.5 w-[66%] rounded-full"></div>
+            </div>
             <label className="block" htmlFor="buy">
               What are you looking to buy?
             </label>
@@ -231,6 +237,9 @@ const Form = () => {
         {/* ----------------------Step 3--------------- */}
         {step === 3 && (
           <div className="card">
+            <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
+              <div className="bg-blue-600 h-2.5 w-[100%] rounded-full"></div>
+            </div>
             <label className="block" htmlFor="employment">
               What is your employment?
             </label>
