f30bb865daff8f1d876f17844003296efef16c29
Vuong Tran <121814098+Vtrandev@users.noreply.github.com>
2023-08-27 20:14:19 -0700

    Contact information on footer updated.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -30,16 +30,17 @@ const Footer = () => {
             <span className="mr-2 mb-2">Facebook</span>
             <span className="mr-2 mb-2">Tiktok</span>
           </div>
-          <p className="mb-2 text-sm">
-            Copyright © 2023 PrimeSolutions. All Rights Reserved.
-          </p>
+          <p className="mb-2 text-sm">Copyright © 2023 PrimeSolutions.</p>
+          <p className="mb-2 text-sm">All Rights Reserved.</p>
         </div>
       </div>
 
       <div className="flex flex-col p-2">
         <h2 className="font-bold mb-2">Contact</h2>
-        <span className="mb-2">Address, Langley, BC V3V 585</span>
-        <span className="mb-2">604-123-4567</span>
+        <span className="mb-2">19335 Langley Bypass, Surrey, BC V3S 6K1</span>
+        <span className="mb-2">BC Team : 604-332-6846</span>
+        <span className="mb-2">AB Team : 825-600-5316</span>
+        <span className="mb-2">ON Team : 647-956-6100</span>
         <p className="mb-2">
           <a href="mailto:email@primesolutions.com">Email us</a>
         </p>
